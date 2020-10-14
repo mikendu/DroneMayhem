e1751d420195069246cd35c682e6f734e6a5e9ac
Michael Ndubuisi <mcn54@cornell.edu>
2020-10-13 21:07:18 -0700

    Basic Unity Setup

---
+++ Python/goto-test.py
@@ -0,0 +1,316 @@
+import cflib.crtp
+from cflib.crazyflie import Crazyflie
+from cflib.crazyflie.mem import LighthouseBsGeometry
+from cflib.crazyflie.mem import MemoryElement
+from cflib.crazyflie.mem import Poly4D
+from cflib.crazyflie.log import LogConfig
+from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
+from cflib.crazyflie.syncLogger import SyncLogger
+
+
+import logging
+import time
+import signal
+import sys
+import keyboard
+import openvr
+import math
+import numpy as np
+
+
+
+# 0 - 255
+brightness = 0
+uri = None
+vr = None
+
+# Only output errors from the logging framework
+logging.basicConfig(level=logging.ERROR)
+
+baseOne = LighthouseBsGeometry()
+baseTwo = LighthouseBsGeometry()
+baseStationIndexes = []
+baseStations = [baseOne, baseTwo]
+dataWritten = False
+height = 0
+
+
+# Rotation matrixes to convert to the CF coordinate system
+openvr_to_cf = np.array([
+    [0.0, 0.0, -1.0],
+    [-1.0, 0.0, 0.0],
+    [0.0, 1.0, 0.0],
+])
+
+cf_to_openvr = np.array([
+    [0.0, -1.0, 0.0],
+    [0.0, 0.0, 1.0],
+    [-1.0, 0.0, 0.0],
+])
+
+DRONE_HEIGHT = 0.05
+SEQUENCE_HEIGHT = 1
+
+# Duration,x^0,x^1,x^2,x^3,x^4,x^5,x^6,x^7,y^0,y^1,y^2,y^3,y^4,y^5,y^6,y^7,z^0,z^1,z^2,z^3,z^4,z^5,z^6,z^7,yaw^0,yaw^1,yaw^2,yaw^3,yaw^4,yaw^5,yaw^6,yaw^7
+figure8 = [
+    [1.050000, 0.000000, -0.000000, 0.000000, -0.000000, 0.830443, -0.276140, -0.384219, 0.180493, -0.000000, 0.000000, -0.000000, 0.000000, -1.356107, 0.688430, 0.587426, -0.329106, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000],  # noqa
+    [0.710000, 0.396058, 0.918033, 0.128965, -0.773546, 0.339704, 0.034310, -0.026417, -0.030049, -0.445604, -0.684403, 0.888433, 1.493630, -1.361618, -0.139316, 0.158875, 0.095799, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000],  # noqa
+    [0.620000, 0.922409, 0.405715, -0.582968, -0.092188, -0.114670, 0.101046, 0.075834, -0.037926, -0.291165, 0.967514, 0.421451, -1.086348, 0.545211, 0.030109, -0.050046, -0.068177, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000],  # noqa
+    [0.700000, 0.923174, -0.431533, -0.682975, 0.177173, 0.319468, -0.043852, -0.111269, 0.023166, 0.289869, 0.724722, -0.512011, -0.209623, -0.218710, 0.108797, 0.128756, -0.055461, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000],  # noqa
+    [0.560000, 0.405364, -0.834716, 0.158939, 0.288175, -0.373738, -0.054995, 0.036090, 0.078627, 0.450742, -0.385534, -0.954089, 0.128288, 0.442620, 0.055630, -0.060142, -0.076163, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000],  # noqa
+    [0.560000, 0.001062, -0.646270, -0.012560, -0.324065, 0.125327, 0.119738, 0.034567, -0.063130, 0.001593, -1.031457, 0.015159, 0.820816, -0.152665, -0.130729, -0.045679, 0.080444, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000],  # noqa
+    [0.700000, -0.402804, -0.820508, -0.132914, 0.236278, 0.235164, -0.053551, -0.088687, 0.031253, -0.449354, -0.411507, 0.902946, 0.185335, -0.239125, -0.041696, 0.016857, 0.016709, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000],  # noqa
+    [0.620000, -0.921641, -0.464596, 0.661875, 0.286582, -0.228921, -0.051987, 0.004669, 0.038463, -0.292459, 0.777682, 0.565788, -0.432472, -0.060568, -0.082048, -0.009439, 0.041158, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000],  # noqa
+    [0.710000, -0.923935, 0.447832, 0.627381, -0.259808, -0.042325, -0.032258, 0.001420, 0.005294, 0.288570, 0.873350, -0.515586, -0.730207, -0.026023, 0.288755, 0.215678, -0.148061, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000],  # noqa
+    [1.053185, -0.398611, 0.850510, -0.144007, -0.485368, -0.079781, 0.176330, 0.234482, -0.153567, 0.447039, -0.532729, -0.855023, 0.878509, 0.775168, -0.391051, -0.713519, 0.391628, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000],  # noqa
+]
+
+class Uploader:
+    def __init__(self):
+        self._is_done = False
+
+    def upload(self, trajectory_mem):
+        print('Uploading data')
+        trajectory_mem.write_data(self._upload_done)
+
+        while not self._is_done:
+            time.sleep(0.2)
+
+    def _upload_done(self, mem, addr):
+        print('Data uploaded')
+        self._is_done = True
+
+
+def initCrazyflie():
+    cflib.crtp.init_drivers()
+    print('Scanning interfaces for Crazyflies...')
+    available = cflib.crtp.scan_interfaces()
+
+    if len(available) == 0:
+        print('No crazyflie found!')
+        exit(0)
+
+    uri = available[0][0]
+    print('Found crazyflie: ', uri)
+    return uri
+
+
+def signal_handler(sig, frame):
+    print('Interrupt signal received, exiting!')
+    sys.exit(0)
+
+
+def setLED(scf): 
+    brightness = round(255 * math.pow(height / 1.5, 2))
+    cf = scf.cf
+
+    # Set virtual mem effect effect
+    cf.param.set_value('ring.effect', '13')
+
+    # Get LED memory and write to it
+    mem = cf.mem.get_mems(MemoryElement.TYPE_DRIVER_LED)
+    if len(mem) > 0:
+        ledMemory = mem[0]
+        for i in range(0, 12): 
+            progress = float(i) / 12
+            red = round(brightness * (1 - progress))
+            blue = round(brightness * progress)
+            ledMemory.leds[i].set(r = red, g = 0, b = blue)
+        
+        ledMemory.write_data(None)
+
+
+def findBaseStations():
+    print('\n\nListing OpenVR Devices...')
+    for i in range(openvr.k_unMaxTrackedDeviceCount):
+        device_class = vr.getTrackedDeviceClass(i)
+        if device_class == openvr.TrackedDeviceClass_TrackingReference:
+            baseStationIndexes.append(i)
+            print('\t- Found Base Station with index: ', i)
+
+
+def getPosition(poseMatrix):
+    rawPosition = [
+        poseMatrix[0][3], 
+        poseMatrix[1][3], 
+        poseMatrix[2][3]
+    ]
+    return np.dot(openvr_to_cf, rawPosition)
+
+def getRotation(poseMatrix): 
+    rawRotation = [poseMatrix[0][:3], poseMatrix[1][:3], poseMatrix[2][:3]]
+    return np.dot(openvr_to_cf, np.dot(rawRotation, cf_to_openvr))
+
+
+def updateBaseStations(cf):
+    maxIndex = max(baseStationIndexes[0], baseStationIndexes[1])
+    poses = vr.getDeviceToAbsoluteTrackingPose(openvr.TrackingUniverseStanding, 0, maxIndex)
+    for i in range(0, maxIndex + 1):
+        if poses[i].bPoseIsValid and i in baseStationIndexes:
+            pose = poses[i]
+            listIndex = baseStationIndexes.index(i)
+            baseStation = baseStations[listIndex]
+            baseStation.origin = getPosition(pose.mDeviceToAbsoluteTracking)
+            baseStation.rotation_matrix = getRotation(pose.mDeviceToAbsoluteTracking)
+
+    writeBaseStationData(cf, baseOne, baseTwo)
+
+
+def dataWrittenCallback(mem, addr):
+    global dataWritten
+    dataWritten = True
+    # print('Data written')
+
+def writeBaseStationData(cf, baseOne, baseTwo):
+    global dataWritten
+    dataWritten = False
+    mems = cf.mem.get_mems(MemoryElement.TYPE_LH)
+    count = len(mems)
+    if count != 1:
+        raise Exception('Unexpected nr of memories found:', count)
+
+    mems[0].geometry_data = [baseOne, baseTwo]
+    mems[0].write_data(dataWrittenCallback)
+
+    while not dataWritten:
+        time.sleep(0.01)
+
+
+def upload_trajectory(cf, trajectory_id, trajectory):
+    trajectory_mem = cf.mem.get_mems(MemoryElement.TYPE_TRAJ)[0]
+
+    total_duration = 0
+    for row in trajectory:
+        duration = row[0]
+        x = Poly4D.Poly(row[1:9])
+        y = Poly4D.Poly(row[9:17])
+        z = Poly4D.Poly(row[17:25])
+        yaw = Poly4D.Poly(row[25:33])
+        trajectory_mem.poly4Ds.append(Poly4D(duration, x, y, z, yaw))
+        total_duration += duration
+
+    Uploader().upload(trajectory_mem)
+    cf.high_level_commander.define_trajectory(trajectory_id, 0,
+                                              len(trajectory_mem.poly4Ds))
+    return total_duration
+
+
+def mainLoop(scf = None):    
+    if (scf is None):
+        print("No crazyflie connection provided, exiting.")
+        exit(0)
+
+    takeoffTime = SEQUENCE_HEIGHT * 1.75
+    cf = scf.cf
+    updateBaseStations(cf)
+    commander = cf.high_level_commander
+    
+    
+    commander.takeoff(1.0, 3.0)
+    time.sleep(3.0)
+
+    commander.go_to(0.0, 0.0, 2.0, 0.0, 3.0)
+    time.sleep(3.0)
+    
+    print("Hovering in position")
+    time.sleep(5.0)
+
+    commander.land(0.1, 3.0)
+    commander.stop()
+
+
+
+
+
+def waitForEstimator(scf):
+    print('Waiting for estimator to find position...')
+
+    log_config = LogConfig(name='Kalman Variance', period_in_ms=500)
+    log_config.add_variable('kalman.varPX', 'float')
+    log_config.add_variable('kalman.varPY', 'float')
+    log_config.add_variable('kalman.varPZ', 'float')
+
+    var_y_history = [1000] * 10
+    var_x_history = [1000] * 10
+    var_z_history = [1000] * 10
+
+    threshold = 0.001
+
+    with SyncLogger(scf, log_config) as logger:
+        for log_entry in logger:
+            data = log_entry[1]
+
+            var_x_history.append(data['kalman.varPX'])
+            var_x_history.pop(0)
+            var_y_history.append(data['kalman.varPY'])
+            var_y_history.pop(0)
+            var_z_history.append(data['kalman.varPZ'])
+            var_z_history.pop(0)
+
+            min_x = min(var_x_history)
+            max_x = max(var_x_history)
+            min_y = min(var_y_history)
+            max_y = max(var_y_history)
+            min_z = min(var_z_history)
+            max_z = max(var_z_history)
+
+            # print("{} {} {}".
+            #       format(max_x - min_x, max_y - min_y, max_z - min_z))
+
+            if (max_x - min_x) < threshold and (
+                    max_y - min_y) < threshold and (
+                    max_z - min_z) < threshold:
+                break
+
+
+def resetEstimator(scf):
+    print('\n')
+    cf = scf.cf
+    cf.param.set_value('kalman.resetEstimation', '1')
+    time.sleep(0.1)
+    cf.param.set_value('kalman.resetEstimation', '0')
+    waitForEstimator(cf)
+
+
+def positionCallback(timestamp, data, logconf):
+    x = data['kalman.stateX']
+    y = data['kalman.stateY']
+    z = data['kalman.stateZ']
+    print('pos: ({}, {}, {})'.format(x, y, z))
+    global height
+    height = z
+
+
+def startPrinting(scf):
+    log_conf = LogConfig(name='Position', period_in_ms=100)
+    log_conf.add_variable('kalman.stateX', 'float')
+    log_conf.add_variable('kalman.stateY', 'float')
+    log_conf.add_variable('kalman.stateZ', 'float')
+
+    scf.cf.log.add_config(log_conf)
+    log_conf.data_received_cb.add_callback(positionCallback)
+    log_conf.start()
+
+
+
+
+
+
+signal.signal(signal.SIGINT, signal_handler)
+vr = openvr.init(openvr.VRApplication_Other)
+findBaseStations()
+print('\n\n')
+
+uri = initCrazyflie()
+with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
+    cf = scf.cf
+    cf.param.set_value('lighthouse.method', '1')
+    cf.param.set_value('stabilizer.controller', '2') # Mellinger controller
+    cf.param.set_value('commander.enHighLevel', '1')    
+    # duration = upload_trajectory(cf, 1, figure8)
+    # print('The sequence is {:.1f} seconds long'.format(duration))
+    updateBaseStations(cf)
+    resetEstimator(scf)
+    # startPrinting(scf)
+    mainLoop(scf)
+    
