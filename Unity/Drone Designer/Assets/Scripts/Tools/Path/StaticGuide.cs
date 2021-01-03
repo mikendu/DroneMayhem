f4bbd6cc99cf970083177a97a74d7e693743c2fd
Michael Ndubuisi <mcn54@cornell.edu>
2021-01-03 03:20:27 -0800

    Static guide editor implemented

--- Unity/Drone Designer/Assets/Scripts/Tools/Path/StaticGuide.cs
+++ Unity/Drone Designer/Assets/Scripts/Tools/Path/StaticGuide.cs
@@ -42,5 +42,36 @@ public class StaticGuide : MonoBehaviour
     {
         //SetShape(ShapeType);
     }
+}
 
+
+[CustomEditor(typeof(StaticGuide))]
+public class StaticGuideEditor: Editor
+{
+    private static readonly string[] options = Enum.GetNames(typeof(ShapeType));
+
+    public override void OnInspectorGUI()
+    {
+        StaticGuide guide = target as StaticGuide;
+        EditorGUILayout.Space(30);
+
+
+        SerializedProperty shape = serializedObject.FindProperty("ShapeType");
+        int currentSelection = shape.enumValueIndex;
+        int newSelection = GUILayout.Toolbar(shape.enumValueIndex, options);
+        //GUILayout.SelectionGrid(currentSelection, options, 5);
+
+        if (newSelection != currentSelection)
+        {
+            guide.SetShape((ShapeType)newSelection);
+            serializedObject.Update();
+            serializedObject.ApplyModifiedProperties();
+        }
+
+
+
+
+
+        EditorGUILayout.Space(30);
+    }
 }
